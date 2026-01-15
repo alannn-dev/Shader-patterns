d6709457de53b68bbc46e8b160e2f016b7ae5cf3
Alan Boulay <boulay.alan@gmail.com>
2026-01-15 13:58:05 +0700

    Pattern n°10

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -22,9 +22,20 @@ void main()
     // float strength = mod(vUv.y * 10.0, 1.0);
 
     // Pattern 8
-    float strength = mod(vUv.y * 10.0, 1.0);
+        // float strength = mod(vUv.y * 10.0, 1.0);
     // if value < 0.5 -> 0 : If value > 0.5 -> 1
-    strength = step(0.5, strength);
+        // strength = step(0.5, strength);
+
+    // Pattern 9
+        // float strength = mod(vUv.y * 10.0, 1.0);
+    // if value < 0.5 -> 0 : If value > 0.5 -> 1
+        // strength = step(0.8, strength);
+
+    // Pattern 10
+        float strength = mod(vUv.x * 10.0, 1.0);
+    // if value < 0.5 -> 0 : If value > 0.5 -> 1
+        strength = step(0.8, strength);
+        
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
