6375f96adb7b71202076710e8ea18e683bdc82e7
Alan Boulay <boulay.alan@gmail.com>
2026-01-15 12:09:57 +0700

    Pattern n°1

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -3,12 +3,15 @@ varying vec2 vUv;
 
 void main()
 {
+    // Pattern 1 
+   /*  float strength = vUv; */
+
     // Pattern 3
     // float strength = vUv.y;
 
     // Pattern 4
-    float strength = vUv.x;
+    // float strength = vUv.x;
 
-    // gl_FragColor = vec4(R, G, B, A);
-    gl_FragColor = vec4(strength, strength, strength, 1.0);
+    // gl_FragColor = vec4(vUv.x, vUv.y, B, A);
+    gl_FragColor = vec4(vUv, 1.0, 1.0);
 }
