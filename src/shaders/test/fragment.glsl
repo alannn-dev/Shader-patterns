74c5dfb097f067d3271c8813f315986045552e15
Alan Boulay <boulay.alan@gmail.com>
2026-01-15 11:40:06 +0700

    First gradient pattern using UV and FragColor

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -3,7 +3,8 @@ varying vec2 vUv;
 
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    gl_FragColor = vec4(vUv, 1.0, 1.0);
+    // gl_FragColor = vec4(vUv.x, vUV.y, 1.0, 1.0);
 
-   //
+
 }
--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -5,5 +5,5 @@ void main()
 {
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 
-     vUv = uv;
+    vUv = uv;
 }
