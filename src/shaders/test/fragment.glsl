0c90ef0a97ba2c5523d33310112a5c1967d118f9
Alan Boulay <boulay.alan@gmail.com>
2026-01-16 11:19:24 +0700

    Pattern n°22

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -88,6 +88,7 @@ void main()
 
     // Pattern 21
     float strength = floor(vUv.x * 10.0) / 10.0;
+    strength *= floor(vUv.y * 10.0) / 10.0;
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
