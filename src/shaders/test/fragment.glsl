23025e1de3c2c0bb69569b2301d35c5e29f7dd78
Alan Boulay <boulay.alan@gmail.com>
2026-01-16 12:44:45 +0700

    Pattern n°33

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -141,10 +141,13 @@ void main()
     // strength *= 0.15 / (distance(vec2(vUv.y, (vUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
 
     // Pattern 32
-    float pi = 3.1415926535897932384626433832795;
+    /* float pi = 3.1415926535897932384626433832795;
     vec2 rotatedUv = rotate(vUv, pi * 0.25, vec2(0.5));
     float strength = 0.15 / (distance(vec2(rotatedUv.x, (rotatedUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
-    strength *= 0.15 / (distance(vec2(rotatedUv.y, (rotatedUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    strength *= 0.15 / (distance(vec2(rotatedUv.y, (rotatedUv.x - 0.5) * 5.0 + 0.5), vec2(0.5))); */
+
+    // Pattern 33
+    float strength = step(0.5, distance(vUv, vec2(0.5)) + 0.25);
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
