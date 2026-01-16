01c55b1eb61cdee7facf050d15671d7761f40e45
Alan Boulay <boulay.alan@gmail.com>
2026-01-16 11:44:00 +0700

    Pattern n°25

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -9,102 +9,97 @@ varying vec2 vUv;
 
 void main()
 {
-    // Pattern 1 
-    // gl_FragColor = vec4(vUv.x, vUv.y, B, A);
+    // // Pattern 1
+    // gl_FragColor = vec4(vUv, 1.0, 1.0);
 
-    // Pattern 3
-    // float strength = vUv.y;
+    // // Pattern 2
+    // gl_FragColor = vec4(vUv, 0.0, 1.0);
 
-    // Pattern 4
+    // // Pattern 3
     // float strength = vUv.x;
 
-    // Pattern 5
-    // float strength = 1.0 - vUv.x;
+    // // Pattern 4
+    // float strength = vUv.y;
 
-    // Pattern 6
+    // // Pattern 5
+    // float strength = 1.0 - vUv.y;
+
+    // // Pattern 6
     // float strength = vUv.y * 10.0;
 
-    // Pattern 7 : MODULO (Create repeating patterns like stripes or checkerboards)
+    // // Pattern 7
     // float strength = mod(vUv.y * 10.0, 1.0);
 
-    // Pattern 8
-        // float strength = mod(vUv.y * 10.0, 1.0);
-    // if value < 0.5 -> 0 : If value > 0.5 -> 1
-        // strength = step(0.5, strength);
+    // // Pattern 8
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    // strength = step(0.5, strength);
 
-    // Pattern 9
-        // float strength = mod(vUv.y * 10.0, 1.0);
-    // if value < 0.5 -> 0 : If value > 0.5 -> 1
-        // strength = step(0.8, strength);
+    // // Pattern 9
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    // strength = step(0.8, strength);
 
-    // Pattern 10
-        // float strength = mod(vUv.x * 10.0, 1.0);
-    // if value < 0.5 -> 0 : If value > 0.5 -> 1
-        // strength = step(0.8, strength);
-        
-    // Pattern 11
+    // // Pattern 10
+    // float strength = mod(vUv.x * 10.0, 1.0);
+    // strength = step(0.8, strength);
+
+    // // Pattern 11
     // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
     // strength += step(0.8, mod(vUv.y * 10.0, 1.0));
+    // strength = clamp(strength, 0.0, 1.0);
 
-    // Pattern 12
+    // // Pattern 12
     // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
     // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
 
-    // Pattern 13
-    // float strength = step(0.2, mod(vUv.x * 10.0, 1.0));
+    // // Pattern 13
+    // float strength = step(0.4, mod(vUv.x * 10.0, 1.0));
     // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
 
-    // Pattern 14
-   /*  float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
-    barX *= step(0.8, mod(vUv.y * 10.0, 1.0));
+    // // Pattern 14
+    // float barX = step(0.4, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+    // float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4, mod(vUv.y * 10.0, 1.0));
+    // float strength = barX + barY;
+    // strength = clamp(strength, 0.0, 1.0);
 
-    float barY = step(0.8, mod(vUv.x * 10.0, 1.0));
-    barY *= step(0.4, mod(vUv.y * 10.0, 1.0));
+    // // Pattern 15
+    // float barX = step(0.4, mod(vUv.x * 10.0 - 0.2, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+    // float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4, mod(vUv.y * 10.0 - 0.2, 1.0));
+    // float strength = barX + barY;
+    // strength = clamp(strength, 0.0, 1.0);
 
-    float strength = barX + barY; */
-
-    // Pattern 15
-    /* float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
-    barX *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
-
-    float barY = step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
-    barY *= step(0.4, mod(vUv.y * 10.0, 1.0));
-
-    float strength = barX + barY; */
-
-    // Pattern 16
-    //Abs() transforms any negative value into its positive counterpart, while positive values ​​remain unchanged.
+    // // Pattern 16
     // float strength = abs(vUv.x - 0.5);
 
-    // Pattern 17
-    // min() : takes two arguments and returns the smaller of the two values.
+    // // Pattern 17
     // float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
 
-    // Pattern 18
-    // max() : takes two arguments and returns the bigger of the two values.
+    // // Pattern 18
     // float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
 
-    // Pattern 19
+    // // Pattern 19
     // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
 
-     // Pattern 20
-    /* float square1 = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
-    float square2 = 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
-    float strength = square1 * square2; */
+    // // Pattern 20
+    // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    // strength *= 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
 
-    // Pattern 21
-   /*  float strength = floor(vUv.x * 10.0) / 10.0;
-    strength *= floor(vUv.y * 10.0) / 10.0; */
+    // // Pattern 21
+    // float strength = floor(vUv.x * 10.0) / 10.0;
 
-    // Pattern 23
-    /* float strength = random(vUv); */
+    // // Pattern 22
+    // float strength = floor(vUv.x * 10.0) / 10.0 * floor(vUv.y * 10.0) / 10.0;
+
+    // // Pattern 23
+    // float strength = random(vUv);
 
     // Pattern 24
-    vec2 gridUv = vec2(
+    /* vec2 gridUv = vec2(
         floor(vUv.x * 10.0) / 10.0,
         floor(vUv.y * 10.0) / 10.0
-    );
+    ); */
 
+    // Pattern 25
+    vec2 gridUv = vec2(floor(vUv.x * 10.0) / 10.0, floor((vUv.y + vUv.x * 0.5) * 10.0) / 10.0);
     float strength = random(gridUv);
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
