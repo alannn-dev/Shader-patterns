2aaec184d4111db79a9f65c9234b47e0cd4258be
Alan Boulay <boulay.alan@gmail.com>
2026-01-16 12:23:23 +0700

    Pattern n°31

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -118,12 +118,17 @@ void main()
     // float strength = 0.015 / (distance(vUv, vec2(0.5)));
 
     // Pattern 30
-    vec2 lightUv = vec2(
+    /* vec2 lightUv = vec2(
         vUv.x * 0.1 + 0.45,
         vUv.y * 0.5 + 0.25
     );
 
-    float strength = 0.015 / distance(lightUv, vec2(0.5));
+    float strength = 0.015 / distance(lightUv, vec2(0.5)); */
+
+    // // Pattern 31
+    float strength = 0.15 / (distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    strength *= 0.15 / (distance(vec2(vUv.y, (vUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
+
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
